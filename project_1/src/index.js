3d356c337dd09072494a791ff9d68682ea483da8
walopes <walopes23@gmail.com>
2020-07-11 21:36:18 -0300

    Added project_1 into the react_play repo

---
+++ project_1/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+// JSX
+ReactDOM.render(
+  <App />,
+//   <h1>Hello World!</h1>,
+  document.getElementById('root')
+);
