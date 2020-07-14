736bb1b17f525549a81256a5d4e4cd9879514f48
walopes <walopes23@gmail.com>
2020-07-13 21:21:25 -0300

    Challenge1 added on index.js

--- project_1/src/index.js
+++ project_1/src/index.js
@@ -4,8 +4,17 @@ import App from './App';
 import './index.css';
 
 // JSX
+
+/*
+ ReactDOM.render(
+   <App />,
+ //   <h1>Hello World!</h1>,
+   document.getElementById('root')
+ );
+*/
+
+// Challenge - Render an unordered list using ReactDOM
 ReactDOM.render(
-  <App />,
-//   <h1>Hello World!</h1>,
-  document.getElementById('root')
+    <ul><li>Item A</li><li>Item B</li><li>Item C</li></ul>,
+    document.getElementById('root')
 );
