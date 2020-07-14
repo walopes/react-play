dd5ee942ad6c57c43b4d27e9d112167b3b517818
walopes <walopes23@gmail.com>
2020-07-13 21:49:20 -0300

    Challenge2 added

--- project_1/src/index.js
+++ project_1/src/index.js
@@ -14,6 +14,25 @@ function MyApplication()
     )
 }
 
+
+function MyInfo()
+{
+    return (
+        <div>
+            <h1>Willian A. Lopes</h1>
+            <p>Hi, I'm learning React! xD</p>
+            <ol>
+                This is my top three vacation places:
+                <li>Egipt</li>
+                <li>Russia</li>
+                <li>Peru</li>
+            </ol>
+        </div>
+    )
+}
+
+
+
 // JSX
 
 //  ReactDOM.render(
@@ -24,6 +43,8 @@ function MyApplication()
 
 // Challenge - Render an unordered list using ReactDOM
 ReactDOM.render(
-    <MyApplication/>,
+    // <MyApplication/>,
+    <MyInfo/>,
     document.getElementById('root')
 );
+
