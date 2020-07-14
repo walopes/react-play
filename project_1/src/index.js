9747e4466043aeba9cc7ac695bfa65d6d8e9c8ba
walopes <walopes23@gmail.com>
2020-07-13 21:41:04 -0300

    Function example added

--- project_1/src/index.js
+++ project_1/src/index.js
@@ -3,18 +3,27 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+function MyApplication()
+{
+    return (
+        <ul>
+            <li>Item A</li>
+            <li>Item B</li>
+            <li>Item C</li>
+        </ul>
+    )
+}
+
 // JSX
 
-/*
- ReactDOM.render(
-   <App />,
- //   <h1>Hello World!</h1>,
-   document.getElementById('root')
- );
-*/
+//  ReactDOM.render(
+//    <App />,
+//  //   <h1>Hello World!</h1>,
+//    document.getElementById('root')
+//  );
 
 // Challenge - Render an unordered list using ReactDOM
 ReactDOM.render(
-    <ul><li>Item A</li><li>Item B</li><li>Item C</li></ul>,
+    <MyApplication/>,
     document.getElementById('root')
 );
