239f282fd7988289d6ac249891ce639ca4e9a69d
walopes <walopes23@gmail.com>
2020-07-14 21:59:05 -0300

    Added component Footer

--- project_1/src/MyInfo.js
+++ project_1/src/MyInfo.js
@@ -1,19 +1,26 @@
 import React from "react"
 
+import Footer from './Footer'
 
 function MyInfo()
 {
     return (
         <div>
-            <h1>Willian A. Lopes</h1>
-            <p>Hi, I'm learning React! xD</p>
-            <ol>
-                {/* This is my top three vacation places: */}
-                <li>Egipt</li>
-                <li>Russia</li>
-                <li>Peru</li>
-            </ol>
-            <a href="http://www.google.com">Google</a>
+            <nav>
+                <h1>Willian A. Lopes</h1>
+                <p>Hi, I'm learning React! xD</p>
+                <ol>
+                    {/* This is my top three vacation places: */}
+                    <li>Egipt</li>
+                    <li>Russia</li>
+                    <li>Peru</li>
+                </ol>
+                <a href="http://www.google.com">Google</a>
+            </nav>
+            <main>
+                <p>This is where the content will go</p>
+            </main>
+            <Footer />
         </div>
     )
 }
